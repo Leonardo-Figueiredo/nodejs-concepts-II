7a6a2b949118886115ab28a5f49cff1f949181ec
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2020-05-01 23:20:02 -0300

    Node.js Concepts II Challenge finalized.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.map(transaction =>
+      transaction.type === 'income' ? transaction.value : 0,
+    );
+
+    const outcome = this.transactions.map(transaction =>
+      transaction.type === 'outcome' ? transaction.value : 0,
+    );
+
+    const incomeBalance =
+      income.length !== 0 ? income.reduce((total, next) => total + next) : 0;
+
+    const outcomeBalance =
+      outcome.length !== 0 ? outcome.reduce((total, next) => total + next) : 0;
+
+    const balance = {
+      income: incomeBalance,
+      outcome: outcomeBalance,
+      total: incomeBalance - outcomeBalance,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
